4f1c06944c5b7b1ac8df1a35691c82e69ad1bb33
Wesley <wesley.custodior@gmail.com>
2022-06-03 00:54:45 -0300

    TESTE

---
+++ M3UEditor/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace M3UEditor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Open = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.SuspendLayout();
+            // 
+            // Open
+            // 
+            this.Open.Location = new System.Drawing.Point(755, 22);
+            this.Open.Name = "Open";
+            this.Open.Size = new System.Drawing.Size(98, 35);
+            this.Open.TabIndex = 0;
+            this.Open.Text = "Open M3U";
+            this.Open.UseVisualStyleBackColor = true;
+            this.Open.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.AllowDrop = true;
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.ColumnWidth = 100;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(55, 38);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(445, 526);
+            this.checkedListBox1.TabIndex = 1;
+            this.checkedListBox1.Visible = false;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(953, 576);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.Open);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Open;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ M3UEditor/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace M3UEditor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                var fileContent = string.Empty;
+                var filePath = string.Empty;
+
+                openFileDialog.InitialDirectory = "D:\\Download";
+                openFileDialog.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    //Get the path of specified file
+                    filePath = openFileDialog.FileName;
+
+                    //Read the contents of the file into a stream
+                    var fileStream = openFileDialog.OpenFile();
+
+                    using (StreamReader reader = new StreamReader(fileStream))
+                    {
+                        fileContent = reader.ReadToEnd();
+
+                        string[] stringSeparators = new string[] { "#EXTINF:-1" };
+                        var links = fileContent.Split(stringSeparators, StringSplitOptions.None);
+                        foreach (var link in links)
+                        {
+                            string[] stringSep = new string[] { "tvg-logo=\"", "\",", "tvg-name=", "group-title=" };
+                            var linksLogo = link.Split(stringSep, StringSplitOptions.None);
+                            if (linksLogo[0].ToString().Contains("#EXTM3U"))
+                                continue;
+                            checkedListBox1.Items.AddRange(new object[]
+                        {
+                            linksLogo[1].ToString()
+                        });
+                            checkedListBox1.Location = new System.Drawing.Point(10, 15);
+                            checkedListBox1.Size = new System.Drawing.Size(120, 150);
+                            this.Controls.Add(checkedListBox1);
+                        }
+                        checkedListBox1.Visible = true;
+                        checkedListBox1.Width = 450;
+                        checkedListBox1.Height = 450;
+                    }
+                }
+            }
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
